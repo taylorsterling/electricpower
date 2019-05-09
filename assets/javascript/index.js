7f55d0247fdcb1bb7fa3419c29d3f913706def3f
Taylor <taylorsterling@icloud.com>
2019-05-08 22:20:43 -0500

    adding SVG graphic

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,13 +1,14 @@
 $(document).ready(function () {
 
-    $("#modalTrigger").click(function(){
-        $(".reveal").fadeIn();
-      });
-
-    $("#closeBtn").click(function(){
-        $(".reveal").fadeOut();
-      });
-
     $(document).foundation();
 
+    // $("#modalTrigger").click(function(){
+    //     $(".reveal").fadeIn();
+    //   });
+    
+    // $("#closeBtn").click(function(){
+    //     $(".reveal").fadeOut();
+    //   });
+
 });
+
