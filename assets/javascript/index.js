daca55a507045e882267a750dfd95951609345bd
Taylor <taylorsterling@icloud.com>
2019-05-09 22:42:17 -0500

    added card modals and animations

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,13 +2,6 @@ $(document).ready(function () {
 
     $(document).foundation();
 
-    // $("#modalTrigger").click(function(){
-    //     $(".reveal").fadeIn();
-    //   });
-    
-    // $("#closeBtn").click(function(){
-    //     $(".reveal").fadeOut();
-    //   });
 
 });
 
